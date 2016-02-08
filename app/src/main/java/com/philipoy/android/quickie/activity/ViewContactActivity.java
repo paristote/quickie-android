e551a6a9241cdb68f43aaa4113e570f2474be284
Philippe Aristote <philippe.aristote@gmail.com>
2016-02-08 15:33:33 +0700

    Add View Contact activity with action to delete the contact

---
+++ app/src/main/java/com/philipoy/android/quickie/activity/ViewContactActivity.java
@@ -0,0 +1,84 @@
+package com.philipoy.android.quickie.activity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.philipoy.android.quickie.R;
+import com.philipoy.android.quickie.model.QuickContact;
+import com.philipoy.android.quickie.storage.QuickContactsDBHelper;
+
+
+public class ViewContactActivity extends AppCompatActivity {
+
+    // TODO: allow editing the contact details or canceling the EOL
+
+    public static final String CONTACT_EXTRA = "Contact_Extra";
+
+    private QuickContact mContact;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_contact);
+        if (getIntent().hasExtra(CONTACT_EXTRA)) {
+            mContact = getIntent().getParcelableExtra(CONTACT_EXTRA);
+            setupActivity();
+        } else {
+            // this activity needs a CONTACT_EXTRA content
+            finish();
+        }
+    }
+
+    private void setupActivity() {
+        TextView contactName, contactPhone, contactEOL;
+        contactName = (TextView)findViewById(R.id.view_contact_name);
+        contactPhone = (TextView)findViewById(R.id.view_contact_phone);
+        contactEOL = (TextView)findViewById(R.id.view_contact_eol);
+        setTitle(mContact.getContactPrimaryName());
+        contactName.setText(mContact.getContactPrimaryName());
+        contactPhone.setText(mContact.getContactPrimaryPhone());
+        contactEOL.setText(mContact.contactEOL);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.view_contact, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_delete_quick_contact) {
+            deleteContactAction();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deleteContactAction() {
+        AlertDialog confirm = new AlertDialog.Builder(this)
+                .setMessage(R.string.title_warning_delete_contact)
+                .setPositiveButton(R.string.action_delete, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        QuickContactsDBHelper dbHelper = new QuickContactsDBHelper(ViewContactActivity.this);
+                        dbHelper.deleteQuickContact(mContact.contactId, true);
+                        Toast.makeText(ViewContactActivity.this, R.string.success_contact_deleted, Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                })
+                .setNegativeButton(R.string.action_cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                })
+                .create();
+
+        confirm.show();
+    }
+}
--- app/src/main/java/com/philipoy/android/quickie/fragment/QuickContactsListFragment.java
+++ app/src/main/java/com/philipoy/android/quickie/fragment/QuickContactsListFragment.java
@@ -1,8 +1,10 @@
 package com.philipoy.android.quickie.fragment;
 
+import com.philipoy.android.quickie.activity.ViewContactActivity;
 import com.philipoy.android.quickie.model.QuickContact;
 import com.philipoy.android.quickie.storage.QuickContactsDBHelper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
@@ -11,7 +13,11 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class QuickContactsListFragment extends ListFragment {
+
+    private ArrayList<QuickContact> items;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,18 +32,18 @@ public class QuickContactsListFragment extends ListFragment {
 	public void onResume() {
 		
 		QuickContactsDBHelper dbHelper = new QuickContactsDBHelper(getActivity());
-		setListAdapter(new ArrayAdapter<QuickContact>(getActivity(), android.R.layout.simple_list_item_1, android.R.id.text1, dbHelper.getAllQuickContacts()));
+        items = dbHelper.getAllQuickContacts();
+		setListAdapter(new ArrayAdapter<QuickContact>(getActivity(), android.R.layout.simple_list_item_1, android.R.id.text1, items));
 		super.onResume();
 	}
 
-
-
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
-		// TODO open a view contact activity to allow editing the contact details, or deleting it, or canceling the EOL
+        QuickContact contact = items.get(position);
+        Intent viewContact = new Intent(getContext(), ViewContactActivity.class);
+        viewContact.putExtra(ViewContactActivity.CONTACT_EXTRA, contact);
+        getContext().startActivity(viewContact);
 		super.onListItemClick(l, v, position, id);
 	}
-
-	
 	
 }
